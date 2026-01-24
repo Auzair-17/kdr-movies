df5608d2951de1f71efea46efe5c88224e06dc4a
Auzair-17 <auzair09270@gmail.com>
2026-01-24 15:01:24 +0430

    Search component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hussle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
   );
 }
 
